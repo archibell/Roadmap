764adba2f0ee6cfbb063fb2ae84ab43b14adc0c8
Lu, Alina Y <alina@gatech.edu>
2026-08-21 16:22:45 -0700

    10Modules

---
+++ Javascript/JSLessons/10Moduesl_main.js
@@ -0,0 +1,7 @@
+import { add, double } from "./10Module_math.js";
+
+const total = add(10, 5);
+const doubledTotal = double(total);
+
+console.log(total);
+console.log(doubledTotal);
---
+++ Javascript/JSLessons/10Modules_math.js
@@ -0,0 +1,7 @@
+export function add(a, b) {
+  return a + b;
+}
+
+export function double(number) {
+  return number * 2;
+}
